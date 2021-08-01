1bc1fe0d77a3014b9f2b6f13a04d9356f5fcfdc3
unknown <hrj7292@gmial.com>
2021-08-01 22:47:47 +0530

    challenges

---
+++ Reading_inputs/src/eg1/Take_Input.java
@@ -0,0 +1,17 @@
+package eg1;
+
+import java.util.Scanner;
+
+public class Take_Input {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name ");
+		String name = sc.nextLine();
+		System.out.println(" enter age");
+		int age = Integer.parseInt(sc.nextLine());
+		System.out.println(" hello" +name);
+		System.out.println(" age = "+ age );
+		
+	}
+
+}
